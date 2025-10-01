52f2b7695aba9b29ddd026c4ea40f48e01efc820
Amogh-007-Rin <amoghdath233@gmail.com>
2025-10-01 23:55:22 +0100

    Stage-2

--- backend.php
+++ backend.php
@@ -1,5 +1,13 @@
+<?php
+// These are some of the examples of data-types in php - php hypertext pre processor.
 $name = "Amogh Dath KA";
 $age = 20;
 $Occupation = "Student";
 
 echo "My name is {$name} and i am a {$occupation}"
+
+
+
+
+?>
+
